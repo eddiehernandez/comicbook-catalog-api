7b696643d3ebe0ad7380959ed48454365f74b931
Eddie Hernandez <eddie.hernandez.us@gmail.com>
2022-06-20 14:39:41 -0400

    import test file

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import config from './config';
 import application from './app';
 import { usersRepo, comicsRepo } from './repos';
 
+import text from './test';
+
 import express from 'express';
 const app = express()
 // console.log(config.port);
@@ -9,7 +11,7 @@ const app = express()
 const PORT : string|number = process.env.PORT || 5000;
 
 app.use("*",(req, res) =>{
-    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+    res.send("<h1>Welcome to your simple server! Awesome right</h1>" + text);
 });
 
 app.listen(PORT,() => console.log(`hosting @${PORT}`));
