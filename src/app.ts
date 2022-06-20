97565753ef9dbbdbd2f5241aa75a045f961bd0ca
Eddie Hernandez <eddie.hernandez.us@gmail.com>
2022-06-20 15:02:37 -0400

    put back original app ts file

--- src/app.ts
+++ src/app.ts
@@ -10,24 +10,24 @@ export default (config: IConfig, usersRepo: IUsersRepo, comicsRepo: IComicsRepo)
 
     const app: Application = express();
 
-    // app.use(express.json());
-    // app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
 
-    // const routes = appRoutes(config, usersRepo, comicsRepo); // inject config and repos into routes
-    // app.use(routes);
+    const routes = appRoutes(config, usersRepo, comicsRepo); // inject config and repos into routes
+    app.use(routes);
 
-    // const swaggerDocument = YAML.load('src/openapi.yaml');
-    // var options = {
-    //     customCss: '.swagger-ui .topbar { display: none }'
-    //   };
-    // app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+    const swaggerDocument = YAML.load('src/openapi.yaml');
+    var options = {
+        customCss: '.swagger-ui .topbar { display: none }'
+      };
+    app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
-    // app.get('*', (req: Request, res: Response) => {
-    //     res.status(404).json({
-    //         code: '404',
-    //         message: `Endpoint: ${req?.originalUrl} not found!`
-    //     });
-    // })
+    app.get('*', (req: Request, res: Response) => {
+        res.status(404).json({
+            code: '404',
+            message: `Endpoint: ${req?.originalUrl} not found!`
+        });
+    })
 
     return app;
 }
